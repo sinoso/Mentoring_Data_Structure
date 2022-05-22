228ec9f0e904f028c9278add6a45dce79aae0edb
sinoso <tpgus123123@naver.com>
2022-05-22 18:23:56 +0900

    Revert "refactor : remove ObservableWithLifeCycle"
    This reverts commit 134c1192f03fda41265f88bd2cca700c1e61d486.

--- src/main/kotlin/observer/Observer.kt
+++ src/main/kotlin/observer/Observer.kt
@@ -1,18 +1,5 @@
 package observer
 
-import lifeCycle.LifeCycle
-import lifeCycle.LifeCycleState
-
-fun <T> Observable<T>.subscribe(lifeCycle: LifeCycle, observer: Observer<T>) {
-    subscribe(observer)
-
-    lifeCycle.addListener {
-        if (it == LifeCycleState.DESTROYED) {
-            unsubscribe(observer)
-        }
-    }
-}
-
 fun interface Observer<T> {
     fun onChange(value: T)
 }
---
+++ src/main/kotlin/observer/ObserverWithLifeCycle.kt
@@ -0,0 +1,35 @@
+package observer
+
+import lifeCycle.LifeCycle
+import lifeCycle.LifeCycleState
+
+fun <T> Observable<T>.subscribe(lifeCycle: LifeCycle, observer: Observer<T>) {
+    val wrapper = ObserverWithLifeCycle(lifeCycle, observer)
+    subscribe(wrapper)
+
+    lifeCycle.addListener {
+        if (it == LifeCycleState.DESTROYED) {
+            unsubscribe(wrapper)
+        }
+    }
+}
+
+class ObserverWithLifeCycle<T>(
+    private val lifeCycle: LifeCycle,
+    private val observer: Observer<T>
+) : Observer<T> {
+
+    override fun onChange(value: T) {
+        if (lifeCycle.isChangeAble()) {
+            observer.onChange(value)
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return observer == other
+    }
+
+    override fun hashCode(): Int {
+        return observer.hashCode()
+    }
+}
